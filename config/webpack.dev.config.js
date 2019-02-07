dc0fb33f213c77c640f799b57314df49bcb29808
Olga Belavina <ol.belavina@gmail.com>
2019-02-06 22:38:25 -0500

    Initial layout

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,19 @@
+const commonPaths = require('./common-paths');
+const webpack = require('webpack');
+
+const config = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: commonPaths.outputPath,
+    compress: true,
+    hot: false,
+    port: 8090,
+    disableHostCheck: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+module.exports = config;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// COMPONENTS
+import App from './components/App';
+
+// STYLES
+import 'normalize.css';
+import './styles/main.scss';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
