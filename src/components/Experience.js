278b78b7d5e5a40fcaee00f5d33b04c3accf48e9
belavina <ol.belavina@gmail.com>
2019-02-10 18:14:41 -0500

    Experience timeline populated from a data source

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,15 +1,72 @@
 import React from 'react';
+import experience from './data/experience';
+
 
 class Experience extends React.Component {
 
+
+  calculateYears = (startDate) => { 
+    const ageDifMs = Date.now() - startDate.getTime();
+    return Math.abs((new Date(ageDifMs)).getUTCFullYear() - 1970);
+  }
+
   render() {
 
+    let breakpoints = [];
+    let dateMarks = [];
+
+    let firstDate = new Date(experience[0].startDate);
+    console.log(firstDate);
+    firstDate.setMonth(firstDate.getMonth() - 3);
+    
+    const timelineSpan = new Date() - firstDate; // 100% of the timeline
+    // let lastDate = 0;
+
+    const calcLeft = (d) => (d - firstDate)*100/timelineSpan;
+
+    experience.forEach((job) => {
+      const startPercentage = calcLeft(new Date(job.startDate))
+      const endPercentage = job.endDate ? calcLeft(new Date(job.endDate)) : 100;
+      breakpoints.push(
+        <div 
+          className="experience__timeline-breakpoint"
+          style={{ 
+            left: `${startPercentage}%`,
+            width: `${endPercentage - startPercentage}%`
+          }}
+        >
+          <h3 className="experience__timeline-job">
+            <span className="experience__timeline-title">{job.title}</span>
+            <span className="experience__timeline-company">{job.company}</span>
+          </h3>
+        </div>
+      );
+    });
+
+    const yearsExperience = this.calculateYears(new Date(firstDate));
+    for (let i=0; i <= yearsExperience; i++) {
+  
+      let markDate = new Date(new Date().getFullYear(), 0, 1);
+      markDate.setFullYear(new Date().getFullYear() - i);
+
+      dateMarks.push(
+        <div 
+          className="experience__timeline-date-mark"
+          style={{ left: `${calcLeft(new Date(markDate))}%` }} 
+        >
+          <span className="experience__timeline-date-year">{markDate.getFullYear()}</span>
+        </div>
+      );
+    }
+    
+    
     return (
       <div className="experience">
         <div className="experience__timeline">
-          <div className="experience__timeline-breakpoint"></div>
-          <div className="experience__timeline-history"></div>
+          {breakpoints}
+          <div className="experience__timeline-history"/>
           <div className="experience__timeline-dates">
+            {dateMarks}
           </div>
         </div>
       </div>
