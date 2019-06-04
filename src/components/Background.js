26c742e2d5cc8677ad6511afa14d8a340d70c8f6
Olga Belavina <ol.belavina@gmail.com>
2019-06-03 21:09:22 -0400

    Video not playing for mobile devices

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Background = () => {
+  let autoPlay = true;
+  if (document.body.clientWidth < 870) {
+    autoPlay = false;
+  }
+
   return (
     <div className="bg-video">
-      <video className="bg-video__content" autoPlay muted loop>
+      <video className="bg-video__content" autoPlay={autoPlay} muted loop>
         <source src={"/videos/particles_720.mp4"} type="video/mp4" />
         Your browser is not supported!
       </video>
